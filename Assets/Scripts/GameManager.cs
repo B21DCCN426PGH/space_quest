5011af12b6736382e2f086b1f1eb3160e9e3cd31
B21DCCN426PGH <161963076+B21DCCN426PGH@users.noreply.github.com>
2026-03-15 15:59:43 +0700

    Obstacles

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public float worldSpeed ;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ObjectSpawners : MonoBehaviour
+{
+    public GameObject prefab;
+    public float spawnTime ;
+    private float spawnInterval;
+    void Update()
+    {
+        
+    }
+}
